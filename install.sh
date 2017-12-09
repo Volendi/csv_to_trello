7a948c40bd549117d0600df97320bf7784e9bebf
Yaroslav Panasenko <yaroslaw.panasencko@yandex.ua>
2017-12-09 16:03:33 +0200

    Added seting credentials in install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,13 @@ done
 
 #Deactivate environment
 deactivate
+
+#Creating credentials file
+credentials_path="csv_to_trello/credentials.py" 
+touch "$credentials_path"
+echo -n "Enter your Trello API key (https://trello.com/app-key) and press [ENTER]: "
+read api_key
+echo "API_KEY = '$api_key'" >> "$credentials_path"
+echo -n "Generate and enter your Trello token and press [ENTER]: "
+read token
+echo "TOKEN = '$token'" >> "$credentials_path"
